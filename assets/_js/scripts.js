55fdada0c355ae8cf12d56829a2c29eb0b45cd52
Raphael Freitas <jraphadeveloper@gmail.com>
2016-07-14 20:47:43 -0300

    first commit - grunt_0.0.1

---
+++ assets/_js/scripts.js
@@ -0,0 +1,22 @@
+(function( window, document, undefined ){
+  var obj = function() {
+    return {
+
+      init : function() {
+        console.log( this.soma_valor( 3, 5 ) );
+      }, // init
+
+
+      soma_valor : function( num1, num2 ) {
+        return num1 + num2;
+      } // soma_valor
+
+    }; // return
+  }; // obj
+
+  obj().init();
+})( window, document );
+
+var teste = (function () {
+   var testando =  console.log("teste");
+});
